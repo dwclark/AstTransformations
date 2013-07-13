8da708096ce58fd22e830508e0d9d7bae3ffb653
David Clark <davidclark@tx.rr.com>
2013-07-13 15:56:01 -0500

    Renamted to MakeAsyncPair.

---
+++ MakeAsyncPair.groovy
@@ -0,0 +1,7 @@
+import org.codehaus.groovy.transform.GroovyASTTransformationClass
+import java.lang.annotation.*
+
+@Retention(RetentionPolicy.SOURCE)
+@Target([ElementType.METHOD])
+@GroovyASTTransformationClass (classes=[MakeAsyncPairTransformation])
+public @interface MakeAsyncPair { }
